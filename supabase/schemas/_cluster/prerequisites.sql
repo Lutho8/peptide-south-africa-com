e26f1f3eef078593a12e5263c1a2ba60c0eefed8
PepSA <orders@peptide-south-africa.com>
2026-09-13 11:12:09 +0200

    Declare baseline role and extension prerequisites

---
+++ supabase/schemas/_cluster/prerequisites.sql
@@ -0,0 +1,9 @@
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'crm_reader') THEN
+    CREATE ROLE crm_reader NOLOGIN;
+  END IF;
+END
+$$;
+
+CREATE SCHEMA IF NOT EXISTS "pgmq";
